ace925741d1fcc12c8a890bdfec27225a9a27ff2
hasuhwan <hapung215@gmail.com>
2022-12-08 15:10:26 +0900

    completed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,59 +1,61 @@
-import React, { useState } from 'react';
-import './App.css';
-import Header from './components/Header';
-import BookTable from './components/BookTable';
-import DisplayBoard from './components/DisplayBoard';
-import CreateBook from './components/CreateBook';
-import { getAllBooks, createBook } from './services/BookService';
-import Footer from './components/Footer';
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import BookTable from "./components/BookTable";
+import DisplayBoard from "./components/DisplayBoard";
+import CreateBook from "./components/CreateBook";
+import { getAllBooks, createBook } from "./services/BookService";
+import Footer from "./components/Footer";
+import { getTodos, createTodo } from "./services/TodoService";
 
-function App () {
-
+function App() {
   const [bookShelf, setBookShelf] = useState({});
   const [books, setBooks] = useState([]);
   const [numberOfBooks, setNumberBooks] = useState(0);
-
+  console.log(getTodos());
+  console.log(
+    createTodo({
+      todo: "산책하기123",
+      category: "운동123",
+      isComplete: true,
+    })
+  );
   const handleSubmit = () => {
-      createBook(bookShelf)
-        .then(() => {
-          setNumberBooks(numberOfBooks+1);
-      });
-  }
+    createBook(bookShelf).then(() => {
+      setNumberBooks(numberOfBooks + 1);
+    });
+  };
 
   const getAllBook = () => {
-    getAllBooks()
-      .then(data => {
-        setBooks(data);
-        setNumberBooks(data.length);
-      });
-  }
+    getAllBooks().then((data) => {
+      setBooks(data);
+      setNumberBooks(data.length);
+    });
+  };
 
   const handleOnChangeForm = (e) => {
-      let inputData = bookShelf;
-      if (e.target.name === 'book') {
-        bookShelf.book = e.target.value;
-      } else if (e.target.name === 'category') {
-        bookShelf.category = e.target.value;
-      } else if (e.target.name === 'author') {
-        bookShelf.author = e.target.value;
-      }
-      setBookShelf(inputData);
-  }
+    let inputData = bookShelf;
+    if (e.target.name === "book") {
+      bookShelf.book = e.target.value;
+    } else if (e.target.name === "category") {
+      bookShelf.category = e.target.value;
+    } else if (e.target.name === "author") {
+      bookShelf.author = e.target.value;
+    }
+    setBookShelf(inputData);
+  };
 
-  
   return (
     <div className="main-wrapper">
       <div className="main">
         <Header />
-        <CreateBook 
+
+        <CreateBook
           bookShelf={bookShelf}
           onChangeForm={handleOnChangeForm}
           handleSubmit={handleSubmit}
         />
-        <DisplayBoard 
-          numberOfBooks={numberOfBooks} 
-          getAllBook={getAllBook} 
-        />
+        <DisplayBoard numberOfBooks={numberOfBooks} getAllBook={getAllBook} />
         <BookTable books={books} />
         <Footer />
       </div>
